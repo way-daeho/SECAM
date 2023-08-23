b4eb983a50b42312e3e10c869fb7569d41112e40
BT-Lee <lavr2755@gmail.com>
2023-08-24 01:14:15 +0900

    log part 설명

--- log/ClientLog.js
+++ log/ClientLog.js
@@ -26,3 +26,10 @@ const ClientLog = new Schema({
 })
 
 module.exports=mongoose.model('ClientLog',ClientLog);
+/*
+* Schema를 생성하기 위한 변수를 mongoose 모듈로 부터 import해줌. const { Schema }
+YMD : 년월일 저장 필수값 -> String형
+time : 시간 저장 시,분,초 -> String형
+status : 발생한 이벤트를(녹화 시장, 종료 등) 저장함 -> String형
+level : 발생한 이벤트의 작동 상태를 저장함 (정상, 비정상) -> String형
+pushCnt : 침입감지 이벤트를 Chart.js를 이용하여 표로 보여주기 위한 변수, 오직 침입감지 이벤트일 때만 1로 저장. -> String형*/ 
--- log/connect.js
+++ log/connect.js
@@ -23,3 +23,11 @@ mongoose.connection.on('disconnected', () => {
 });
 
 module.exports = connect;
+
+/* 코드 설명
+  const mongoose = require('mongoose') 
+  mongoose를 쓰기 위해 모듈 'mongoose'를 상수형 변수 Mongoose에 선언
+  const connect함수 => 함수과 실행되면, callback함수를 호출한다. mongoose.connect를 사용하여, 입력한 DB아이디와 PW, 그리고 포트번호
+  와 사용자의 계정 직위를 입력을 입력하면 해당 계정에 연결을 한다, 그 이후 dbName은 SECAM에 연결하는 함수이다. 연결이 잘 되었다면 then()함수 호출,
+  에러 발생 시, 몽고디비 연결 에러 메세지와 발생한 에러를 출력해준다.
+  해당 모듈을 app.js에서 사용하기 위해 module.exports를 이용하여 app.js에서 connect라는 함수로 사용할 수 있게 한다.*/
